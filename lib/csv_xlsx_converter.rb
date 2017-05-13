e0b560ff64e0c75a873bb6761428849a10735302
William Yeung <william@tofugear.com>
2017-05-13 10:20:49 +0800

    add proper bin command csv_to_xlsx; add bom handling for UTF8

--- lib/csv_xlsx_converter.rb
+++ lib/csv_xlsx_converter.rb
@@ -5,7 +5,7 @@ require_relative "csv_xlsx_converter/filename"
 require_relative "csv_xlsx_converter/converter"
 require_relative "csv_xlsx_converter/cli"
 
-
-if __FILE__ == $0
-  CsvXlsxConverter::main
-end
+# we have moved this to bin/csv_to_xlsx for proper bin installation
+# if __FILE__ == $0
+#   CsvXlsxConverter::main
+# end
